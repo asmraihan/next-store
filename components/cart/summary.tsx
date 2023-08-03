1a0b387dd26d7371814ea41218beb57188241f64
asmraihan <asmraihanctg14@gmail.com>
2023-08-04 00:17:36 +0600

    fixes

--- components/cart/summary.tsx
+++ components/cart/summary.tsx
@@ -12,12 +12,30 @@ import { Button } from '../ui/button';
 
 
 const Summary = () => {
-    const SearchParams = useSearchParams()
+    const searchParams = useSearchParams()
     const items = useCart((state) => state.items)
     const removeAll = useCart((state) => state.removeAll)
+
+    useEffect(() => {
+        if(searchParams.get("success")){
+            toast.success("Payment was successful")
+            removeAll()
+        }
+        if(searchParams.get("canceled")){
+            toast.error("Payment was canceled")
+        }
+    }, [searchParams, removeAll])
+
     const totalPrice = items.reduce((total, item) => {
         return total + Number(item.price)
     }, 0)
+
+    const onCheckout  = async () => {
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
+            productIds: items.map((item) => item.id)
+        })
+        window.location = response.data.url
+    }
     return (
         <div className='mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 '>
             <h2 className='text-lg font-medium text-gray-900'>Order Summary</h2>
@@ -30,9 +48,11 @@ const Summary = () => {
                 </div>
             </div>
             <Button
+                disabled={items.length === 0}
                 variant="default"
                 size="lg"
                 className='w-full mt-6 bg-black'
+                onClick={onCheckout}
             >
                 Checkout
             </Button>
